4a8b3ebc16916fb1ce591e4564206ac6fd5141c9
spghljh <spghljh@gmail.com>
2025-10-13 21:08:25 +0900

    main-251013_2108

--- src/main/java/kr/co/mdi2/Mdi2Application.java
+++ src/main/java/kr/co/mdi2/Mdi2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Mdi2BootPstmtLegacyMigrationApplication {
+public class Mdi2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Mdi2BootPstmtLegacyMigrationApplication.class, args);
+		SpringApplication.run(Mdi2Application.class, args);
 	}
 
 }
